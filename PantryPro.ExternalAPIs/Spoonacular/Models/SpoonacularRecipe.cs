8cecff0b2ad372aa406f0a843108c487fef49684
davidmachadoPY <avidmachado@gmail.com>
2025-06-15 16:28:58 -0500

    feat(ci): Configuracion del entorno completa.

---
+++ PantryPro.ExternalAPIs/Spoonacular/Models/SpoonacularRecipe.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PantryPro.ExternalAPIs.Spoonacular.Models
+{
+    public class SpoonacularRecipe
+    {
+        
+    }
+}
